340dbe3fb3addcfeb2b188c89b4ce03f011fd691
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-02-19 14:40:40 +0200

    Create Messaging.java

---
+++ Fundamentals/ListsMoreExercise/Messaging.java
@@ -0,0 +1,36 @@
+package ListsMoreExercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Messaging {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> numbers = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        StringBuilder message = new StringBuilder(scanner.nextLine());
+        StringBuilder decryptedMessage = new StringBuilder();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            String getNumbers = numbers.get(i);
+            int number = Integer.parseInt(getNumbers);
+            int getIndex = 0;
+            while (number > 0) {
+                getIndex += number % 10;
+                number /= 10;
+            }
+            while (getIndex > message.length()) {
+                getIndex -= message.length();
+            }
+            String getChar = String.valueOf(message.charAt(getIndex));
+            decryptedMessage.append(getChar);
+            message.deleteCharAt(getIndex);
+        }
+        System.out.println(decryptedMessage);
+    }
+}
