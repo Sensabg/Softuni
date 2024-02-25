c9eb9163a5e8483d67eb58babdd8a5311c36df2e
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-02-25 16:20:18 +0200

    Add files via upload

---
+++ Fundamentals/ObjectsAndClasses/students2/Student.java
@@ -0,0 +1,48 @@
+package ObjectsAndClasses.students2;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String hometown;
+
+    public Student(String firstName, String lastName, int age, String hometown) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+}
