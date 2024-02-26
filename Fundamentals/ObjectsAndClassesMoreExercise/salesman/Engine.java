33d50bf57994865f2af514145f30c9738d0747d6
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-02-26 08:26:26 +0200

    Update Engine.java

--- Fundamentals/ObjectsAndClassesMoreExercise/salesman/Engine.java
+++ Fundamentals/ObjectsAndClassesMoreExercise/salesman/Engine.java
@@ -16,37 +16,20 @@ public class Engine {
     public String getEngineModel() {
         return engineModel;
     }
-
-    public void setEngineModel(String engineModel) {
-        this.engineModel = engineModel;
-    }
-
+    
     public String getPower() {
         return power;
     }
-
-    public void setPower(String power) {
-        this.power = power;
-    }
-
+    
     public String getDisplacement() {
         return displacement;
     }
-
-    public void setDisplacement(String displacement) {
-        this.displacement = displacement;
-    }
-
+    
     public String getEfficiency() {
         return efficiency;
     }
-
-    public void setEfficiency(String efficiency) {
-        this.efficiency = efficiency;
-    }
-
+    
     public String toString() {
         return String.format("%s %s %s %s", this.engineModel, this.power, this.displacement, this.efficiency);
     }
-
 }
