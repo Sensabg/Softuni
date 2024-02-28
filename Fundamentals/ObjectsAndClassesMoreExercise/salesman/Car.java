25f6534c169602f9dfb7fa73862ff8c1f60a93f5
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-02-28 16:06:13 +0200

    Update Car.java

--- Fundamentals/ObjectsAndClassesMoreExercise/salesman/Car.java
+++ Fundamentals/ObjectsAndClassesMoreExercise/salesman/Car.java
@@ -1,15 +1,31 @@
 package ObjectsAndClassesMoreExercise.salesman;
 
 public class Car {
-    String carModel;
-    Engine engine;
-    String weight;
-    String color;
+    private  String carModel;
+    private  Engine engine;
+    private  String weight;
+    private  String color;
 
     public Car(String carModel, Engine engine, String weight, String color) {
         this.carModel = carModel;
         this.engine = engine;
         this.weight = weight;
+        this.color = color;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
 
@@ -27,5 +43,9 @@ public class Car {
 
     public String toString() {
         return String.format("%s %s %s %s", this.carModel, this.engine, this.weight, this.color);
+    }
+
+    public Engine getEngine() {
+        return engine;
     }
 }
