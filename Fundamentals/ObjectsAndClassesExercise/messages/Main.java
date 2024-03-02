c8eb713ad0216e953bb1114940c2031f8f8fdce3
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-03-02 14:01:09 +0200

    Add files via upload

---
+++ Fundamentals/ObjectsAndClassesExercise/messages/Main.java
@@ -0,0 +1,18 @@
+package ObjectsAndClassesExercise.messages;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Message message = new Message();
+
+        for (int i = 0; i < n; i++) {
+            String randomMessage = message.randomMessage();
+            System.out.println(randomMessage);
+        }
+    }
+}
