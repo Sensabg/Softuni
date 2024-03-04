090c71130c643c39a5df6ba2a663ae46206ca93a
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-03-04 23:07:07 +0200

    Update Judge.java

--- Fundamentals/AssociativeArraysLambdaAndStreamAPIMoreExercise/Judge.java
+++ Fundamentals/AssociativeArraysLambdaAndStreamAPIMoreExercise/Judge.java
@@ -1,52 +1,53 @@
 package AssociativeArraysLambdaAndStreamAPIMoreExercise;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Judge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Map<String, Integer>> usersData = new TreeMap<>();
+        Map<String, Map<String, Integer>> contestsParticipants = new LinkedHashMap<>();
         String input;
 
-        while (!(input = scanner.nextLine()).equals("no more time")) {
-            String[] inputData = input.split(" -> ");
-            String student = inputData[0];
-            String course = inputData[1];
-            int points = Integer.parseInt(inputData[2]);
+        while (!"no more time".equals(input = scanner.nextLine())) {
+            String[] tokens = input.split(" -> ");
+            String username = tokens[0];
+            String course = tokens[1];
+            int userPoints = Integer.parseInt(tokens[2]);
 
-            usersData.computeIfAbsent(course, k -> new TreeMap<>())
-                    .put(student, points);
+            contestsParticipants.putIfAbsent(course, new HashMap<>());
+            contestsParticipants.get(course)
+                    .compute(username, (user, oldPoints) ->
+                            (oldPoints == null || oldPoints < userPoints) ? userPoints : oldPoints);
         }
-        AtomicInteger iteration = new AtomicInteger(1);
 
-        Comparator<Map.Entry<String, Map<String, Integer>>> courseComparator =
-                Comparator.<Map.Entry<String, Map<String, Integer>>>comparingInt(entry -> entry.getValue().size())
-                        .reversed();
+        Comparator<Map.Entry<String, Integer>> participantsComparator = Map.Entry.<String, Integer>
+                        comparingByValue(Comparator.reverseOrder())
+                .thenComparing(Map.Entry.comparingByKey());
 
-        usersData.entrySet().stream()
-                .sorted(courseComparator)
-                .forEach(courseEntry -> {
-                    String course = courseEntry.getKey();
-                    Map<String, Integer> points = courseEntry.getValue();
-                    iteration.set(1);
-
-                    System.out.printf("%s: %d participants\n", course, points.size());
-
-                    points.entrySet().stream()
-                            .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                            .forEach(e -> System.out.printf("%d. %s <::> %d\n", iteration.getAndIncrement(), e.getKey(), e.getValue()));
-                });
-        iteration.set(1);
+        contestsParticipants.forEach((contest, participants) -> {
+            System.out.printf("%s: %d participants\n", contest, participants.size());
+            final int[] index = {0};
+            participants.entrySet()
+                    .stream()
+                    .sorted(participantsComparator)
+                    .forEach(participant -> {
+                        System.out.printf("%d. %s <::> %d%n",
+                                ++index[0], participant.getKey(), participant.getValue());
+                    });
+        });
         System.out.println("Individual standings:");
-        Map<String, Integer> totalPoints = usersData.entrySet().stream()
-                .flatMap(entry -> entry.getValue().entrySet().stream())
-                .collect(Collectors.groupingBy(Map.Entry::getKey,
-                        Collectors.summingInt(Map.Entry::getValue)));
-
-        totalPoints.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .forEach(e -> System.out.printf("%d. %s -> %d\n", iteration.getAndIncrement(), e.getKey(), e.getValue()));
+        final int[] index = {0};
+        contestsParticipants.values()
+                .stream()
+                .flatMap(m -> m.entrySet().stream())
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)))
+                .entrySet()
+                .stream()
+                .sorted(participantsComparator)
+                .forEach(participant -> {
+                    System.out.printf("%d. %s -> %d%n", ++index[0], participant.getKey(), participant.getValue());
+                });
     }
 }
