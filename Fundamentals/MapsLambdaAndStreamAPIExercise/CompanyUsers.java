946bff5ab2eeb355f33d45a157132916798fd9a9
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-03-04 05:47:36 +0200

    Add files via upload

---
+++ Fundamentals/MapsLambdaAndStreamAPIExercise/CompanyUsers.java
@@ -0,0 +1,28 @@
+package MapsLambdaAndStreamAPIExercise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CompanyUsers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, List<String>> company = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+
+            String[] info = input.split(" -> ");
+            String name= info[0];
+            String id = info[1];
+
+            company.putIfAbsent(name, new ArrayList<>());
+            company.get(name).add(id);
+
+            input = scanner.nextLine();
+        }
+        company.entrySet().forEach(entry ->
+                System.out.printf("%s\n-- %s\n", entry.getKey(),
+                        entry.getValue().stream().map(String::valueOf).collect(Collectors.joining("\n-- "))));
+    }
+}
