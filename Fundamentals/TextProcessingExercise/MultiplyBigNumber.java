582f016e8ae35a03a18b3b849ab179b4d1feb71a
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-03-10 17:46:37 +0200

    Add files via upload

---
+++ Fundamentals/TextProcessingExercise/MultiplyBigNumber.java
@@ -0,0 +1,15 @@
+package TextProcessingExercise;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class MultiplyBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger num1 = new BigInteger(scanner.nextLine());
+        BigInteger num2 = new BigInteger(scanner.nextLine());
+
+        System.out.println(num1.multiply(num2));
+    }
+}
