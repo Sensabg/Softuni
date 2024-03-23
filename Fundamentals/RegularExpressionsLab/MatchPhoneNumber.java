f85f58eca543cc6005a02774735084e28c9bfc2e
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-03-23 09:02:17 +0200

    Update MatchPhoneNumber.java

--- Fundamentals/RegularExpressionsLab/MatchPhoneNumber.java
+++ Fundamentals/RegularExpressionsLab/MatchPhoneNumber.java
@@ -1,28 +1,24 @@
 package RegularExpressionsLab;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MatchDates_03 {
-
+public class MatchPhoneNumber {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
+        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
         String text = scanner.nextLine();
-
-        String regex = "\\b(?<day>\\d{2})([\\.\\-\\/])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
 
-        while (matcher.find()) {
-            String wholeMatch = matcher.group(); 
-            String day = matcher.group("day"); 
-            String month = matcher.group("month"); 
-            String year = matcher.group("year"); 
-
-            System.out.printf("Day: %s, Month: %s, Year: %s\n", day, month, year);
+        List<String> validNumbers = new ArrayList<>();
+        while (matcher.find()){
+            validNumbers.add(matcher.group());
         }
+        System.out.println(String.join(", ", validNumbers));
     }
 }
