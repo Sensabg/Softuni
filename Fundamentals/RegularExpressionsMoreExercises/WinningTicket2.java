5a65a3ad64f6a4a3641e299843f7c551fb480e40
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-03-30 14:01:47 +0200

    Update WinningTicket2.java

--- Fundamentals/RegularExpressionsMoreExercises/WinningTicket2.java
+++ Fundamentals/RegularExpressionsMoreExercises/WinningTicket2.java
@@ -5,16 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WinningTicket2 {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String[] tickets = scanner.nextLine().split("[,\\s]+");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] tickets = scanner.nextLine().split("[,\\s]+");
 
-            for (String ticket : tickets) {
-                if (ticket.length() != 20) {
-                    System.out.println("invalid ticket");
-                    continue;
-                }
-
+        for (String ticket : tickets) {
+            
+            if (ticket.length() != 20) {
+                System.out.println("invalid ticket");
+            } else {
                 String pattern = "([$|@|#|\\^]{6,})[\\s\\S]+?\\1";
                 Matcher matcher = Pattern.compile(pattern).matcher(ticket.substring(0, 10) + " " + ticket.substring(10));
 
@@ -22,12 +21,11 @@ public class WinningTicket2 {
                     char symbol = matcher.group(1).charAt(0);
                     int count = matcher.group(1).length();
 
-                    System.out.printf("ticket \"%s\" - %d%c%s%n", ticket, count, symbol, (count == 10) ? " Jackpot!" : "");
+                    System.out.printf("ticket \"%s\" - %d%c%s\n", ticket, count, symbol, (count == 10) ? " Jackpot!" : "");
                 } else {
-                    System.out.println("ticket \"" + ticket + "\" - no match");
+                    System.out.printf("ticket \"%s\" - no match\n", string);
                 }
             }
         }
     }
-
 }
