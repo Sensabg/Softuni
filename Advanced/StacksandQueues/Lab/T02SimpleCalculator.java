e3c7b66275d834c0f55b7630a723e9c939f7f60b
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-05-01 13:53:52 +0300

    Add files via upload

---
+++ Advanced/StacksandQueues/Lab/T02SimpleCalculator.java
@@ -0,0 +1,37 @@
+package StacksQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class T02SimpleCalculator {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        Collections.addAll(stack, input);
+
+        while (stack.size() > 1){
+            int numberOne = Integer.parseInt(stack.pop());
+            String symbol = stack.pop();
+            int numberTwo = Integer.parseInt(stack.pop());
+            int result = 0;
+
+            switch (symbol){
+
+                case "+":
+                    result = numberOne + numberTwo;
+                    break;
+
+                case "-":
+                    result = numberOne - numberTwo;
+                    break;
+            }
+            stack.push(String.valueOf(result));
+        }
+        System.out.println(stack.peek());
+    }
+}
