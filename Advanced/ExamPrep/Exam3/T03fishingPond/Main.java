06653cbcbdf5326f75349a35a8668df725069e4a
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-06-12 18:15:03 +0300

    Add files via upload

---
+++ Advanced/ExamPrep/Exam3/T03fishingPond/Main.java
@@ -0,0 +1,27 @@
+package ExamPrep.Exam3.T03fishingPond;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pond pond = new Pond(5);
+
+        Fish trout = new Fish("Trout", 5, "winter");
+        Fish perch = new Fish("Perch", 2, "summer");
+        Fish pike = new Fish("Pike", 4, "spring");
+        Fish catfish = new Fish("Catfish", 8, "summer");
+
+        pond.addFish(trout);
+        pond.addFish(perch);
+        pond.addFish(pike);
+
+        System.out.println(pond.removeFish("Trout"));
+        System.out.println(pond.removeFish("Carp"));
+        System.out.println(pike.getMatingSeason());
+
+        pond.addFish(catfish);
+        System.out.println(pond.getOldestFish().getSpecies());
+
+        System.out.println(pond.getVacancies());
+        System.out.println(pond.report());
+    }
+}
---
+++ Advanced/ExamPrep/Exam3/T03fishingPond/Pond.java
@@ -0,0 +1,56 @@
+package ExamPrep.Exam3.T03fishingPond;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pond {
+    private int capacity;
+    private List<Fish> fishes;
+
+    // Constructor
+    public Pond(int capacity) {
+        this.capacity = capacity;
+        this.fishes = new ArrayList<>();
+    }
+
+    // Method to add a fish
+    public void addFish(Fish fish) {
+        if (fishes.size() < capacity) {
+            fishes.add(fish);
+        }
+    }
+
+    // Method to remove a fish by species
+    public boolean removeFish(String species) {
+        return fishes.removeIf(fish -> fish.getSpecies().equals(species));
+    }
+
+    // Method to get the oldest fish
+    public Fish getOldestFish() {
+        return fishes.stream().max((fish1, fish2) -> Integer.compare(fish1.getAge(), fish2.getAge())).orElse(null);
+    }
+
+    // Method to get a fish by species
+    public Fish getFish(String species) {
+        return fishes.stream().filter(fish -> fish.getSpecies().equals(species)).findFirst().orElse(null);
+    }
+
+    // Method to get the count of fishes
+    public int getCount() {
+        return fishes.size();
+    }
+
+    // Method to get the number of vacancies in the pond
+    public int getVacancies() {
+        return capacity - fishes.size();
+    }
+
+    // Method to report fishes
+    public String report() {
+        StringBuilder report = new StringBuilder("Fishes in the pond:\n");
+        for (Fish fish : fishes) {
+            report.append(fish.toString()).append("\n");
+        }
+        return report.toString().trim();
+    }
+}
