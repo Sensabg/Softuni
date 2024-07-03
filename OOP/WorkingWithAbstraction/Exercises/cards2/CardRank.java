a30ba0fbbcfc0fe23458c7baafba43c2a1c8d1ef
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-07-03 05:36:01 +0300

    Create CardRank.java

---
+++ OOP/WorkingWithAbstraction/Exercises/cards2/CardRank.java
@@ -0,0 +1,17 @@
+package cards2;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
