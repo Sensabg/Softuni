1ea8dce40b3c5bbc711f7407d4e3168a5cb99599
🎛️🟰🎛️🟰🎛️🟰🎛️🟰🎛️🟰🎛️ㅤㅤ ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤDaniel Dinevㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ🎛️🟰🎛️🟰🎛️🟰🎛️🟰🎛️🟰🎛️ㅤㅤㅤㅤㅤ <151548889+Sensabg@users.noreply.github.com>
2024-07-15 01:28:24 +0300

    Update and rename OnTimeForTheExamSlavi.java to OnTimeForTheExam-OPTIMIZED.java

---
+++ AdditionalExercises/HelpingFriends/OnTimeForTheExam-OPTIMIZED.java
@@ -0,0 +1,38 @@
+package PersonalTasks;
+
+import java.util.Scanner;
+
+public class OnTimeNew {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int examStartInMinutes = readAndConvert(scanner);
+        int arrivalMinutes = readAndConvert(scanner);
+
+        int timeDiffInMinutes = arrivalMinutes - examStartInMinutes;
+
+        int hours = Math.abs(timeDiffInMinutes / 60);
+        int minutes = Math.abs(timeDiffInMinutes % 60);
+
+        String formattedTime = (hours == 0) ?
+                String.format("%01d minutes", minutes) :
+                String.format("%d:%02d hours", hours, minutes);
+
+        if (timeDiffInMinutes > 30) {
+            System.out.printf("Late %s after the start", formattedTime);
+        } else if (Math.abs(timeDiffInMinutes) < 30) {
+            System.out.printf("Early %s before the start", formattedTime);
+        } else {
+            System.out.println("On time");
+
+            if (timeDiffInMinutes != 0) {
+                String direction = (timeDiffInMinutes < 0) ? "before" : "after";
+                System.out.printf("%s %s the start", formattedTime, direction);
+            }
+        }
+    }
+
+    private static int readAndConvert(Scanner scanner) {
+        return Integer.parseInt(scanner.nextLine()) * 60 + Integer.parseInt(scanner.nextLine());
+    }
+}
