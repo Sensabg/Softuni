e048896ba7f1d11ad58d4ccf8ab6f162ed722db3
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-08-06 12:48:17 +0300

    Update Outdoor.java

--- OOP/ExamPrep/handball/entities/gameplay/Outdoor.java
+++ OOP/ExamPrep/handball/entities/gameplay/Outdoor.java
@@ -2,6 +2,7 @@ package handball.entities.gameplay;
 
 public class Outdoor extends BaseGameplay {
     private static final int CAPACITY = 150;
+
     public Outdoor(String name) {
         super(name, CAPACITY);
     }
