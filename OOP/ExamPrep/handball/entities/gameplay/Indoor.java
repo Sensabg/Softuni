8f61645526577ee66dcd34a930169a46d0819f55
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-08-06 12:48:04 +0300

    Update Indoor.java

--- OOP/ExamPrep/handball/entities/gameplay/Indoor.java
+++ OOP/ExamPrep/handball/entities/gameplay/Indoor.java
@@ -2,6 +2,7 @@ package handball.entities.gameplay;
 
 public class Indoor extends BaseGameplay {
     private static final int CAPACITY = 250;
+
     public Indoor(String name) {
         super(name, CAPACITY);
     }
