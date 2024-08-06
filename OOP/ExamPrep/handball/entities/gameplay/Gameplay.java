58337e0070f4f6ac0c439e90766799bed4e22cc4
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-08-06 12:47:20 +0300

    Update Gameplay.java

--- OOP/ExamPrep/handball/entities/gameplay/Gameplay.java
+++ OOP/ExamPrep/handball/entities/gameplay/Gameplay.java
@@ -7,20 +7,11 @@ import java.util.Collection;
 
 public interface Gameplay {
     int allProtection();
-
     void addTeam(Team team);
-
     void removeTeam(Team team);
-
     void addEquipment(Equipment equipment);
-
     void teamsInGameplay();
-
     Collection<Team> getTeam();
-
     Collection<Equipment> getEquipments();
-
     String getName();
-
-
 }
