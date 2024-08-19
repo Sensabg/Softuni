d82f8babd25fbc5dc770114111ddc1ff499324c0
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-08-19 23:42:41 +0300

    Add files via upload

---
+++ AdditionalExercises/HelpingFriends/matchingBracket.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class matchingBracket {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cycles = Integer.parseInt(scanner.nextLine());
+
+        int countOpenBrackets = 0;
+        boolean balanced = true;
+
+        while (cycles-- > 0) {
+            String input = scanner.nextLine();
+
+            if (input.equals("(")) {
+                countOpenBrackets++;
+            } else if (input.equals(")")) {
+                countOpenBrackets--;
+                if (countOpenBrackets < 0) {
+                    balanced = false;
+                    break;
+                }
+            }
+        }
+
+        if (countOpenBrackets != 0) {
+            balanced = false;
+        }
+
+
+        if(balanced){
+            System.out.println("BALANCED");
+        } else {
+            System.out.println("UNBALANCED");
+        }
+
+       // System.out.println(balanced ? "BALANCED" : "UNBALANCED");
+    }
+}
