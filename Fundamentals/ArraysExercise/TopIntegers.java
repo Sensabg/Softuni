a879bbfe911ddea16a70140089c94ddd1e799339
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-08-26 18:28:32 +0300

    Update TopIntegers.java

--- Fundamentals/ArraysExercise/TopIntegers.java
+++ Fundamentals/ArraysExercise/TopIntegers.java
@@ -7,10 +7,9 @@ public class TopIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] numbers = Arrays
-                .stream(scanner.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                              .mapToInt(Integer::parseInt)
+                              .toArray();
 
         for (int i = 0; i < numbers.length - 1; i++) {
             int currentNum = numbers[i];
@@ -23,6 +22,7 @@ public class TopIntegers {
                     break;
                 }
             }
+
             if (!isBigger) {
                 System.out.print(currentNum + " ");
             }
