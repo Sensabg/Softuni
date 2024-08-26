2d7489bf57c57344bf3773ec1402063b9f9628cb
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-08-26 18:29:02 +0300

    Update TopIntegersTwo.java

--- Fundamentals/ArraysExercise/TopIntegersTwo.java
+++ Fundamentals/ArraysExercise/TopIntegersTwo.java
@@ -7,10 +7,9 @@ public class TopIntegersTwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] numbers = Arrays
-                .stream(scanner.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+    int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                              .mapToInt(Integer::parseInt)
+                              .toArray();
 
         for (int j = 0; j < numbers.length - 1; j++) {
             if (numbers[j] > numbers[j + 1] && numbers[numbers.length - 1] < numbers[j]){
