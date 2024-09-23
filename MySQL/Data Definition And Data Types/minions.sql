7871ce7ea5aa51277ad4423ffac35bf748a1f974
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-09-23 09:27:42 +0300

    Update minions.sql

--- MySQL/Data Definition And Data Types/minions.sql
+++ MySQL/Data Definition And Data Types/minions.sql
@@ -16,3 +16,28 @@ ADD COLUMN town_id INT,
 ADD CONSTRAINT fk_town
 FOREIGN KEY (town_id) REFERENCES towns(id);
 
+DROP TABLE IF EXISTS minions;
+DROP TABLE IF EXISTS towns;
+
+CREATE TABLE towns (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE minions (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(50) NOT NULL, 
+    age INT, 
+    town_id INT,
+    CONSTRAINT fk_town FOREIGN KEY (town_id) REFERENCES towns(id)
+);
+
+INSERT INTO towns (name) VALUES 
+    ('Sofia'), 
+    ('Plovdiv'), 
+    ('Varna');
+
+INSERT INTO minions (name, age, town_id) VALUES 
+    ('Kevin', 22, 1), 
+    ('Bob', 15, 3), 
+    ('Steward', NULL, 2);
