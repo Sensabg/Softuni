694487c55b0a5c607e825b7f610415ca744dd5ae
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-09-23 05:08:02 +0300

    Create gamebar.sql

---
+++ MySQL/Data Definition And Data Types/gamebar.sql
@@ -0,0 +1,18 @@
+USE gamebar;
+
+CREATE TABLE products (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    category_id INT NOT NULL
+);
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
+);
