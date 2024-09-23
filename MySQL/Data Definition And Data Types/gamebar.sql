b9e32c62ed9aa38e2ede6df812d39fc59a591955
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-09-23 10:13:45 +0300

    Update gamebar.sql

--- MySQL/Data Definition And Data Types/gamebar.sql
+++ MySQL/Data Definition And Data Types/gamebar.sql
@@ -1,112 +1,95 @@
-CREATE DATABASE gamebar;
+-- 1. -------------------------------------------------------------------------------------  
+        CREATE DATABASE gamebar;
 
-USE gamebar;
+        USE gamebar;
+-- 2. -------------------------------------------------------------------------------------   
+        CREATE TABLE products (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(50) NOT NULL,
+            category_id INT NOT NULL
+        );
 
-CREATE TABLE products (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    category_id INT NOT NULL
-);
+        CREATE TABLE categories (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(50) NOT NULL
+        );  
+    
+        CREATE TABLE employees (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL
+        );
+-- 3. -------------------------------------------------------------------------------------  
+        SELECT * FROM gamebar.employees;
 
-CREATE TABLE categories (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL
-);
+        INSERT INTO employees (first_name, last_name) VALUES 
+            ('Petrohan', 'Troharev'), 
+            ('Palamud', 'Karakudov'), 
+            ('Salzan', 'Naplakanov')
+            ('Gosho', 'Muhamedov'),
+            ('Ivan', 'Pleshkov'),
+            ('Grogonil', 'Karakalkanov');
 
-CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
-);
+        INSERT INTO employees (first_name, last_name) VALUES 
+            ('Petrohan', 'Troharev'), 
+            ('Palamud', 'Karakudov'), 
+            ('Salzan', 'Naplakanov')
+            ('Gosho', 'Muhamedov'),
+            ('Ivan', 'Pleshkov'),
+            ('Grogonil', 'Karakalkanov');
 
-SELECT * FROM gamebar.employees;
+        INSERT INTO employees (first_name, last_name) VALUES 
+            ('Petrohan', 'Troharev'), 
+            ('Palamud', 'Karakudov'), 
+            ('Salzan', 'Naplakanov')
+            ('Gosho', 'Muhamedov'),
+            ('Ivan', 'Pleshkov'),
+            ('Grogonil', 'Karakalkanov');
+-- 4. -------------------------------------------------------------------------------------  
+        DELETE FROM employees
+        WHERE id IN (1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-INSERT INTO employees (first_name, last_name) VALUES 
-    ('Petrohan', 'Troharev'), 
-    ('Palamud', 'Karakudov'), 
-    ('Salzan', 'Naplakanov')
+        DELETE FROM employees;
+        TRUNCATE TABLE employees;
 
-SELECT * FROM gamebar.employees;
+        ALTER TABLE employees AUTO_INCREMENT = 1;  
+-- 5. -------------------------------------------------------------------------------------  
+        INSERT INTO employees (first_name, last_name) VALUES 
+            ('Petrohan', 'Troharev'), 
+            ('Palamud', 'Karakudov'), 
+            ('Salzan', 'Naplakanov')
+            ('Gosho', 'Muhamedov'),
+            ('Ivan', 'Pleshkov'),
+            ('Grogonil', 'Karakalkanov');
 
-INSERT INTO employees (first_name, last_name) VALUES
-    ('Gosho', 'Muhamedov'),
-    ('Ivan', 'Pleshkov'),
-    ('Grogonil', 'Karakalkanov');
+        ALTER TABLE employees
+        ADD COLUMN middle_name VARCHAR(50);
 
-SELECT * FROM gamebar.employees;
+        ALTER TABLE products
+        ADD CONSTRAINT fk_category
+        FOREIGN KEY (category_id) REFERENCES categories(id);
+-- 6. -------------------------------------------------------------------------------------  
+        SELECT * FROM gamebar.employees;
 
-INSERT INTO employees (first_name, last_name) VALUES 
-    ('Petrohan', 'Troharev'), 
-    ('Palamud', 'Karakudov'), 
-    ('Salzan', 'Naplakanov')
+        DESCRIBE employees;
 
-SELECT * FROM gamebar.employees;
+        ALTER TABLE employees
+        MODIFY middle_name VARCHAR(100);
 
-INSERT INTO employees (first_name, last_name) VALUES
-    ('Gosho', 'Muhamedov'),
-    ('Ivan', 'Pleshkov'),
-    ('Grogonil', 'Karakalkanov');
+        DESCRIBE employees;
+-- 7. -------------------------------------------------------------------------------------  
+        ALTER TABLE employees
+        MODIFY first_name VARCHAR(100),
+        MODIFY last_name VARCHAR(100),
 
-SELECT * FROM gamebar.employees;
+        DESCRIBE employees;
 
-INSERT INTO employees (first_name, last_name) VALUES 
-    ('Petrohan', 'Troharev'), 
-    ('Palamud', 'Karakudov'), 
-    ('Salzan', 'Naplakanov')
+        SELECT * FROM gamebar.employees;
 
-SELECT * FROM gamebar.employees;
+        ALTER TABLE employees
+        DROP COLUMN middle_name
 
-INSERT INTO employees (first_name, last_name) VALUES
-    ('Gosho', 'Muhamedov'),
-    ('Ivan', 'Pleshkov'),
-    ('Grogonil', 'Karakalkanov');
+        SELECT * FROM gamebar.employees;
 
-USE gamebar;
-
-DELETE FROM employees
-WHERE id IN (1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-DELETE FROM employees;
-TRUNCATE TABLE employees;
-
-ALTER TABLE employees AUTO_INCREMENT = 1;  
-
-INSERT INTO employees (first_name, last_name) VALUES
-    ('Petrohan', 'Troharev'), 
-    ('Palamud', 'Karakudov'), 
-    ('Salzan', 'Naplakanov'),
-    ('Gosho', 'Muhamedov'),
-    ('Ivan', 'Pleshkov'),
-    ('Grogonil', 'Karakalkanov');
-
-ALTER TABLE employees
-ADD COLUMN middle_name VARCHAR(50);
-
-ALTER TABLE products
-ADD CONSTRAINT fk_category
-FOREIGN KEY (category_id) REFERENCES categories(id);
-
-SELECT * FROM gamebar.employees;
-
-DESCRIBE employees;
-
-ALTER TABLE employees
-MODIFY middle_name VARCHAR(100);
-
-DESCRIBE employees;
-
-ALTER TABLE employees
-MODIFY first_name VARCHAR(100),
-MODIFY last_name VARCHAR(100),
-
-DESCRIBE employees;
-
-SELECT * FROM gamebar.employees;
-
-ALTER TABLE employees
-DROP COLUMN middle_name
-
-SELECT * FROM gamebar.employees;
-
-ALTER TABLE employees
-ADD COLUMN middle_name VARCHAR(100);
+        ALTER TABLE employees
+        ADD COLUMN middle_name VARCHAR(100);
