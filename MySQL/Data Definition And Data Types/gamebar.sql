e4a583377114290e63e126d4ea76550b45d378a1
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-09-23 05:53:04 +0300

    Update gamebar.sql

--- MySQL/Data Definition And Data Types/gamebar.sql
+++ MySQL/Data Definition And Data Types/gamebar.sql
@@ -33,4 +33,49 @@ INSERT INTO employees (first_name, last_name) VALUES
     ('Ivan', 'Pleshkov'),
     ('Grogonil', 'Karakalkanov');
 
+SELECT * FROM gamebar.employees;
+
+INSERT INTO employees (first_name, last_name) VALUES 
+    ('Petrohan', 'Troharev'), 
+    ('Palamud', 'Karakudov'), 
+    ('Salzan', 'Naplakanov')
+
+SELECT * FROM gamebar.employees;
+
+INSERT INTO employees (first_name, last_name) VALUES
+    ('Gosho', 'Muhamedov'),
+    ('Ivan', 'Pleshkov'),
+    ('Grogonil', 'Karakalkanov');
+
+SELECT * FROM gamebar.employees;
+
+INSERT INTO employees (first_name, last_name) VALUES 
+    ('Petrohan', 'Troharev'), 
+    ('Palamud', 'Karakudov'), 
+    ('Salzan', 'Naplakanov')
+
+SELECT * FROM gamebar.employees;
+
+INSERT INTO employees (first_name, last_name) VALUES
+    ('Gosho', 'Muhamedov'),
+    ('Ivan', 'Pleshkov'),
+    ('Grogonil', 'Karakalkanov');
+
+USE gamebar;
+
+DELETE FROM employees
+WHERE id IN (1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+DELETE FROM employees;
+TRUNCATE TABLE employees;
+
+ALTER TABLE employees AUTO_INCREMENT = 1;  
+
+INSERT INTO employees (first_name, last_name) VALUES
+    ('Petrohan', 'Troharev'), 
+    ('Palamud', 'Karakudov'), 
+    ('Salzan', 'Naplakanov'),
+    ('Gosho', 'Muhamedov'),
+    ('Ivan', 'Pleshkov'),
+    ('Grogonil', 'Karakalkanov');
 
