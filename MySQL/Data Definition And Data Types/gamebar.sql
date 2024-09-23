f140b14577798befd2b4cf983e3bd7d54ad3235a
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-09-23 06:20:30 +0300

    Update gamebar.sql

--- MySQL/Data Definition And Data Types/gamebar.sql
+++ MySQL/Data Definition And Data Types/gamebar.sql
@@ -4,19 +4,19 @@ USE gamebar;
 
 CREATE TABLE products (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     category_id INT NOT NULL
 );
 
 CREATE TABLE categories (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE employees (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
 );
 
 SELECT * FROM gamebar.employees;
@@ -85,3 +85,18 @@ ADD COLUMN middle_name VARCHAR(50);
 ALTER TABLE products
 ADD CONSTRAINT fk_category
 FOREIGN KEY (category_id) REFERENCES categories(id);
+
+SELECT * FROM gamebar.employees;
+
+DESCRIBE employees;
+
+ALTER TABLE employees
+MODIFY middle_name VARCHAR(100);
+
+DESCRIBE employees;
+
+ALTER TABLE employees
+MODIFY first_name VARCHAR(100),
+MODIFY last_name VARCHAR(100),
+
+DESCRIBE employees;
