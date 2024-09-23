5ba9040232b6c620c3a22102ff85c691dcefe0d0
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-09-23 08:25:55 +0300

    Create minions.sql

---
+++ MySQL/Data Definition And Data Types/minions.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA Minions;
+
+CREATE TABLE minions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT
+);
+
+CREATE TABLE towns (
+    town_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
