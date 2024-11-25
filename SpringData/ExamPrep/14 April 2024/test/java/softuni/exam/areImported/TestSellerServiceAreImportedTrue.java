47aa19e5ed003b473b4eaffd59b7cf716e07a0f2
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-11-25 05:17:03 +0200

    Add files via upload

---
+++ SpringData/ExamPrep/14 April 2024/test/java/softuni/exam/areImported/TestSellerServiceAreImportedTrue.java
@@ -0,0 +1,26 @@
+//package softuni.exam.areImported;
+////TestSellerServiceAreImportedTrue
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import softuni.exam.repository.SellerRepository;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class TestSellerServiceAreImportedTrue {
+//
+//    @InjectMocks
+//    private SellerServiceImpl sellerService;
+//    @Mock
+//    private SellerRepository mockSellerRepository;
+//
+//    @Test
+//    void areImportedShouldReturnFalse() {
+//        Mockito.when(mockSellerRepository.count()).thenReturn(1L);
+//        Assertions.assertTrue(sellerService.areImported());
+//    }
+//}
