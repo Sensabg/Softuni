1057180df4af75be679142f98c4b37625f1bbd91
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-11-25 05:04:44 +0200

    Delete SpringData/ExamPrep/14_April_2024 directory

--- SpringData/ExamPrep/14_April_2024/src/main/java/softuni/exam/models/entity/DeviceType.java
+++
@@ -1,5 +0,0 @@
-package softuni.exam.models.entity;
-
-public enum DeviceType {
-    SMART_PHONE, TABLET, SMART_WATCH, LAPTOP
-}
--- SpringData/ExamPrep/14_April_2024/src/test/resources/cleanup.sql
+++
@@ -1,3 +0,0 @@
-TRUNCATE TABLE devices;
-TRUNCATE TABLE sales;
-TRUNCATE TABLE sellers;
