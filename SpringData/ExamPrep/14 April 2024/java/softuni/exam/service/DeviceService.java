c96dccce175ccdef54ff57be5e78fcf286b15666
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-11-25 05:25:01 +0200

    Update DeviceService.java

--- SpringData/ExamPrep/14 April 2024/java/softuni/exam/service/DeviceService.java
+++ SpringData/ExamPrep/14 April 2024/java/softuni/exam/service/DeviceService.java
@@ -4,14 +4,13 @@ package softuni.exam.service;
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 
-// TODO: Implement all methods
 public interface DeviceService {
 
     boolean areImported();
 
     String readDevices() throws IOException;
 
-	String importDevices() throws IOException, JAXBException;
+    String importDevices() throws IOException, JAXBException;
 
     String exportDevices();
 
